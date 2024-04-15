bdc18dfd4a95035aa00fe34bfa38a9c6af33c288
WangYCheng23 <ucdcheng@ucdavis.edu>
2024-04-16 00:27:53 +0800

    new file:   .vscode/launch.json 	modified:   Env.py

--- Env.py
+++ Env.py
@@ -1,47 +1,18 @@
+import os
+import random
 from assembly import OCCAssembly
 import gym
 import numpy as np
 
 
-class Env(gym):  # 定义一个名为Env的类，表示装配体的环境
+class Env(gym.Env):  # 定义一个名为Env的类，表示装配体的环境
 
-    def __init__(self, step_filename):  # 定义环境
+    def __init__(self, step_files):  # 定义环境
+        self.step_filenames = step_files
 
-        self.assembly = OCCAssembly(step_filename)  # 读取模型文件
-        self.step_filename = step_filename
-        self.assembly.create_boom()  # 创建装配模型的爆炸视图，用于显示零件的装配顺序。
-
-        self.part_num = self.assembly.get_part_num()  # 获取装配模型中的零件数量
-
-        self.assembly.compute_countij()  # 提前计算每个零件排在在某个其他零件后发生碰撞次数，为了加速
-
-        self.n_actions = self.part_num  # 将动作空间的大小设置为零件的数量，表示每个动作是选择一个零件进行装配
-
-        self.stepedparts = []  # 已装配的
-
-        self.unstepparts = []  # 未装配的
-
-        self.maxabsx = max([abs(self.assembly.boom_transform[i].direction[0])
-                           for i in range(self.part_num)])+1e-3
-
-        self.maxabsy = max([abs(self.assembly.boom_transform[i].direction[1])
-                           for i in range(self.part_num)])+1e-3
-
-        self.maxabsz = max([abs(self.assembly.boom_transform[i].direction[2])
-                           for i in range(self.part_num)])+1e-3
-        # 计算出每个零件的外围包裹立方体的顶点的xyz的坐标的绝对值最大值，用于后面做状态特征的归一化
-        self.maxabscornnerx = max([abs(self.assembly.bboxes[i].CornerMax().X()) for i in range(
-            self.part_num)]+[abs(self.assembly.bboxes[i].CornerMin().X()) for i in range(self.part_num)])+1e-3
-
-        self.maxabscornnery = max([abs(self.assembly.bboxes[i].CornerMax().Y()) for i in range(
-            self.part_num)]+[abs(self.assembly.bboxes[i].CornerMin().Y()) for i in range(self.part_num)])+1e-3
-
-        self.maxabscornnerz = max([abs(self.assembly.bboxes[i].CornerMax().Z()) for i in range(
-            self.part_num)]+[abs(self.assembly.bboxes[i].CornerMin().Z()) for i in range(self.part_num)])+1e-3
-
-        self.n_state = self.part_num*2*9
 
     def get_state(self):  # 将装配体的状态转换成一个适合神经网络处理的向量形式
+        """states design"""
 
         unsteppartstate = [self.assembly.boom_transform[id].direction+[self.assembly.bboxes[id].CornerMin().X(), self.assembly.bboxes[id].CornerMax().X(), self.assembly.bboxes[id].CornerMin().Y(), self.assembly.bboxes[id].CornerMax().Y(),
                                                                        self.assembly.bboxes[id].CornerMin().Z(), self.assembly.bboxes[id].CornerMax().Z()] for id in self.unstepparts]  # 获取每个未装配好零件的装配方向向量以及包裹立方体的顶点的坐标的最大最小值
@@ -78,6 +49,7 @@ class Env(gym):  # 定义一个名为Env的类，表示装配体的环境
         return state
 
     def comp_fit(self, one_path):
+        """reward func"""
         # print(one_path)
         if len(one_path) == 0:
             return 0
@@ -100,13 +72,42 @@ class Env(gym):  # 定义一个名为Env的类，表示装配体的环境
 
         return interference_count * 5 + 5 * direction_change_num
 
-    def reset(self):  # 环境重置，
+    def reset(self, seed=23):  # 环境重置，
+        step_filename = random.choice(self.step_filenames)
+        
+        self.assembly = OCCAssembly(step_filename)  # 读取模型文件
+        self.step_filename = step_filename
+        self.assembly.create_boom()  # 创建装配模型的爆炸视图，用于显示零件的装配顺序。
 
-        self.stepedparts = []  # 装配好的零件集合置空
+        self.part_num = self.assembly.get_part_num()  # 获取装配模型中的零件数量
 
-        self.unstepparts = list(range(self.part_num))  # 未装配好零件按照顺序
+        self.assembly.compute_countij()  # 提前计算每个零件排在在某个其他零件后发生碰撞次数，为了加速
 
-        self.stepedparts = []
+        self.n_actions = self.part_num  # 将动作空间的大小设置为零件的数量，表示每个动作是选择一个零件进行装配
+
+        self.stepedparts = []  # 已装配的
+
+        self.unstepparts = []  # 未装配的
+
+        self.maxabsx = max([abs(self.assembly.boom_transform[i].direction[0])
+                           for i in range(self.part_num)])+1e-3
+
+        self.maxabsy = max([abs(self.assembly.boom_transform[i].direction[1])
+                           for i in range(self.part_num)])+1e-3
+
+        self.maxabsz = max([abs(self.assembly.boom_transform[i].direction[2])
+                           for i in range(self.part_num)])+1e-3
+        # 计算出每个零件的外围包裹立方体的顶点的xyz的坐标的绝对值最大值，用于后面做状态特征的归一化
+        self.maxabscornnerx = max([abs(self.assembly.bboxes[i].CornerMax().X()) for i in range(
+            self.part_num)]+[abs(self.assembly.bboxes[i].CornerMin().X()) for i in range(self.part_num)])+1e-3
+
+        self.maxabscornnery = max([abs(self.assembly.bboxes[i].CornerMax().Y()) for i in range(
+            self.part_num)]+[abs(self.assembly.bboxes[i].CornerMin().Y()) for i in range(self.part_num)])+1e-3
+
+        self.maxabscornnerz = max([abs(self.assembly.bboxes[i].CornerMax().Z()) for i in range(
+            self.part_num)]+[abs(self.assembly.bboxes[i].CornerMin().Z()) for i in range(self.part_num)])+1e-3
+
+        self.n_state = self.part_num*2*9
 
     def step(self, action):
 
@@ -132,12 +133,8 @@ class Env(gym):  # 定义一个名为Env的类，表示装配体的环境
     
 
 if __name__ == '__main__':
-
-    step_filename = 'assembly.step'
-
-    env = Env(step_filename)
+    train_dir = '/home/wangc/Documents/rl4occ/data/train'
+    step_filenames = [os.path.join(train_dir, path) for path in os.listdir(train_dir)]
+    env = Env(step_filenames)
     env.reset()
 
-    env.step(2)
-    env.step(5)
-    env.step(4)
